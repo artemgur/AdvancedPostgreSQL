59380c11f1e083c35dac5f1411dfc3e292722671
artemgur <armgnv@gmail.com>
2022-02-20 21:43:08 +0300

    Update FillFactorPgBench.sh, FillFactorPgBenchSelect.sh, and 3 more files...

--- PostgreSQL Task 1/FillFactor/PgBenchSelectCleanTables.sh
+++ PostgreSQL Task 1/FillFactor/PgBenchSelectCleanTables.sh
@@ -1,4 +1,4 @@
 # -t 100 — 100 транзакций
 # -r — показать время выполнения отдельных операторов
 # -f выполнить скрипт из файла
-echo postgres | su - postgres -c "pgbench -t 100 -rf '/home/artemgur/Git/GitHub/AdvancedPostgreSQL/PostgreSQL Task 1/FillFactor/PgBenchSelect.sql'"
+echo postgres | su - postgres -c "pgbench -t 1000 -rf '/home/artemgur/Git/GitHub/AdvancedPostgreSQL/PostgreSQL Task 1/FillFactor/PgBenchSelect.sql'"
