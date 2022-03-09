bc3cba7e479add190b0b75a42a0983a74b9efc77
artemgur <armgnv@gmail.com>
2022-03-09 23:25:37 +0300

    Update SelectContinuous.sql, SelectSeparate.sql, and 2 more files...

--- Task 3 (Cluster)/SelectContinuous.sql
+++ Task 3 (Cluster)/SelectContinuous.sql
@@ -1,3 +1,3 @@
-WITH counter AS (SELECT nextval('pgbench_sequence') AS a)
+WITH counter AS (SELECT mod(nextval('pgbench_sequence') * 40, 10000) AS a)
 SELECT id, name FROM task3, counter
 WHERE id BETWEEN counter.a AND counter.a + 39
