17bfd13f5b682e186da7ebe05ef53a1f5d65d911
artemgur <armgnv@gmail.com>
2022-03-22 18:36:07 +0300

    Update main.py

--- Task 5 (Partitioned Table)/PartitionDataDistribution/main.py
+++ Task 5 (Partitioned Table)/PartitionDataDistribution/main.py
@@ -4,12 +4,12 @@ import psycopg2
 partition_count = 10
 
 connection = psycopg2.connect(dbname='postgres', user='postgres', password='postgres', host='localhost')
-cursor = connection.cursor()
-partition_size_list = []
-for i in range(partition_count):
-    cursor.execute(f"SELECT count(*) AS row_count FROM partition{i}")
-    partition_size_list.append(cursor.fetchone()[0])
+with connection.cursor() as cursor:
+    partition_size_list = []
+    for i in range(partition_count):
+        cursor.execute(f"SELECT count(*) AS row_count FROM partition{i}")
+        partition_size_list.append(cursor.fetchone()[0])
 
-plt.bar(range(partition_count), partition_size_list)
-#plt.show()
-plt.savefig("Graph.png", format="PNG")
+    plt.bar(range(partition_count), partition_size_list)
+    #plt.show()
+    plt.savefig("Graph.png", format="PNG")
