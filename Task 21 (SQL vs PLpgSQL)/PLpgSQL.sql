e437c117eb3aa7ec465cb3b47ef550dfdb93ebd3
artemgur <armgnv@gmail.com>
2022-05-14 22:32:23 +0300

    Update PLpgSQL.sql, PgBenchPLpgSQL.sh, and PgBenchSQL.sh

--- Task 21 (SQL vs PLpgSQL)/PLpgSQL.sql
+++ Task 21 (SQL vs PLpgSQL)/PLpgSQL.sql
@@ -1,5 +1,3 @@
--- noinspection SpellCheckingInspection
-
 CREATE OR REPLACE FUNCTION abc_plpgsql(x int) RETURNS int AS $$
 BEGIN
     RETURN (SELECT b FROM abc WHERE a = x LIMIT 1);
--- Task 21 (SQL vs PLpgSQL)/PgBenchSQL.sh
+++ Task 21 (SQL vs PLpgSQL)/PgBenchSQL.sh
@@ -1,1 +1,1 @@
-pgbench -U postgres -t 10000 -nc 1 -f PgBenchSQL.sql
+pgbench -U postgres -t 100000 -nc 1 -f PgBenchSQL.sql
