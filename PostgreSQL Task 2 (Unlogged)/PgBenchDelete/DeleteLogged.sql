82ea0fae134b8eef03dc3c13d88824d3e0ffab1c
artemgur <artemgur01@gmail.com>
2022-06-04 22:44:12 +0300

    Cleanup

--- PostgreSQL Task 2 (Unlogged)/PgBenchDelete/DeleteLogged.sql
+++ PostgreSQL Task 2 (Unlogged)/PgBenchDelete/DeleteLogged.sql
@@ -1,8 +1,3 @@
 WITH counter AS (SELECT nextval('pgbench_sequence') AS a)
 DELETE FROM logged
-	WHERE EXISTS (SELECT a FROM counter WHERE a=id) --TODO likely there is a better way
-
--- Версия с SELECT id FROM table_name LIMIT 1
--- WITH some_id AS (SELECT id AS id2 FROM logged LIMIT 1)
--- DELETE FROM logged
--- 	WHERE EXISTS (SELECT id2 FROM some_id WHERE id2=id) --TODO likely there is a better way
+	WHERE EXISTS (SELECT a FROM counter WHERE a=id)
