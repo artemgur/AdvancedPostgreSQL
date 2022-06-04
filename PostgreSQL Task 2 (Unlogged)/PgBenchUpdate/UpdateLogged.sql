26b4c7911b35428077d0e6dd3d7405254f9ab3cb
artemgur <artemgur01@gmail.com>
2022-06-04 22:38:04 +0300

    Cleanup

--- PostgreSQL Task 2 (Unlogged)/PgBenchUpdate/UpdateLogged.sql
+++ PostgreSQL Task 2 (Unlogged)/PgBenchUpdate/UpdateLogged.sql
@@ -1,15 +1,4 @@
---\set sdf debug(:client_id)
---\setshell sdf echo $my_var
---\set dsfljk debug(:sdf)
---SELECT :client_id FROM fillfactor50 LIMIT 1
-
--- UPDATE logged
--- 	SET time = clock_timestamp()
--- 	WHERE id=nextval('pgbench_sequence')
-
---TODO
 WITH counter AS (SELECT nextval('pgbench_sequence') AS a)
 UPDATE logged
-	--SET time = clock_timestamp()
 	SET string = '1234567890'
-	WHERE EXISTS (SELECT a FROM counter WHERE a=id) --TODO likely there is a better way
+	WHERE EXISTS (SELECT a FROM counter WHERE a=id)
--- PostgreSQL Task 2 (Unlogged)/PgBenchUpdate/UpdateUnlogged.sql
+++ PostgreSQL Task 2 (Unlogged)/PgBenchUpdate/UpdateUnlogged.sql
@@ -1,16 +1,4 @@
---\set sdf debug(:client_id)
---\setshell sdf echo $my_var
---\set dsfljk debug(:sdf)
---SELECT :client_id FROM fillfactor50 LIMIT 1
-
--- UPDATE unlogged
--- 	SET time = clock_timestamp()
--- 	WHERE id=nextval('pgbench_sequence')
-
-
---TODO
 WITH counter AS (SELECT nextval('pgbench_sequence') AS a)
 UPDATE unlogged
-	--SET time = clock_timestamp()
 	SET string = '1234567890'
-	WHERE EXISTS (SELECT a FROM counter WHERE a=id) --TODO likely there is a better way
+	WHERE EXISTS (SELECT a FROM counter WHERE a=id)
